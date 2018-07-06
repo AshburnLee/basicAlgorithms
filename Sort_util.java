2850fac7abfc2919feb549676e9e0df960ed792a
AshburnLee <1578034415@qq.com>
2018-07-06 14:26:11 +0100

    utitlity functionss

--- Sort_util.java
+++ Sort_util.java
@@ -1,13 +1,13 @@
 package myProj;
 
 public class Sort_util {
-
+    // print an array 
     public static void printArray(int[] array){
         for(int i=0;i<array.length;i++){
             System.out.println(array[i]);
         }
     }
-
+    // generate an array with random elements
     public static int[] randomArray(int size){
         int[] array = new int[size];
         for (int i=0;i<size;i++) {
